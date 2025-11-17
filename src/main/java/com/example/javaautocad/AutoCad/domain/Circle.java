1cb2fcd147aae814a7350667856a1c04c9405d83
sayoun555 <zzangsang523@gmail.com>
2025-11-17 20:11:24 +0900

    feat: 둘레 구하는 기능 구현

--- src/main/java/com/example/javaautocad/AutoCad/domain/Circle.java
+++ src/main/java/com/example/javaautocad/AutoCad/domain/Circle.java
@@ -2,5 +2,26 @@ package com.example.javaautocad.AutoCad.domain;
 
 public class Circle {
     private final Point point;
-    private final double radius
+    private final double radius;
+
+    public Circle(Point point, double radius) {
+        this.point = point;
+        this.radius = radius;
+    }
+
+    public double circumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public double curvature() {
+        return 1.0 / radius;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
 }
